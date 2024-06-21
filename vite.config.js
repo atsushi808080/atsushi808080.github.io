e9c5a3a3492068f3afcdcc626bba8319c48727c0
Neil <neil@mail.tca.org.tw>
2024-06-21 14:21:06 +0800

    ok----

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/my-react-app/', // 替換 'my-react-app' 為你的 GitHub 存儲庫名稱
-  build: {
-    outDir: 'dist' // 指定輸出目錄（通常是 'dist'）
-  }
+  plugins: [react()],
+  base: "test",
 });
